61f2526031e9b02acfa646fa2eb8790eb1731168
keerthan <keerthanbommana2003@gmail.com>
2026-02-25 10:12:00 +0530

    Initial Spring Boot Jenkins Demo

---
+++ src/test/java/org/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
